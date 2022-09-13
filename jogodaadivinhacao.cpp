8ed9741037870eb3fcf8a19616ae2018dfb7b23f
rookie-leo <proleonarpa@gmail.com>
2022-09-12 21:56:29 -0300

    Primeiro programa

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout <<"**************************************" << endl;
+    cout <<"* Bem-vindos ao jogo da adivinhação! *" << endl;
+    cout <<"**************************************" << endl;
+}
