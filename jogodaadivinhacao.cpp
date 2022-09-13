a0f9b878d19b6c668ddbf93dc9c62c93cbc17230
rookie-leo <proleonarpa@gmail.com>
2022-09-12 22:14:00 -0300

    Armazenando um numero secreto

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -5,4 +5,7 @@ int main() {
     cout <<"**************************************" << endl;
     cout <<"* Bem-vindos ao jogo da adivinhação! *" << endl;
     cout <<"**************************************" << endl;
+
+    int numero_secreto = 45;
+    cout <<"O número secreto é " <<numero_secreto<<".Não conte para ningúem!" << endl;
 }
