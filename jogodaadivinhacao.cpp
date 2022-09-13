d5d72bcc446ef0420ca20cc6d0da4fb5f2607bd5
rookie-leo <proleonarpa@gmail.com>
2022-09-13 07:07:35 -0300

    Fazendo chutes até acertar

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,19 +8,34 @@ int main() {
 
     const int NUMERO_SECRETO = 45;
     int chute;
+    int tentativas = 0;
+    bool nao_acertou = true;
 
-    cout << "Digite o valor do seu chute: "; //"cout <<" Saida de dados pelo terminal
-    cin >> chute; // "cin >>" Entrada de dados pelo terminal
-    cout << "O valor digitado foi " <<chute <<endl;
+    while (nao_acertou) {
+        tentativas++;
 
-    bool maior = chute > NUMERO_SECRETO;
-    bool menor = chute < NUMERO_SECRETO;
+        cout << "Tentativa " <<tentativas <<endl;
+        cout << "Digite o valor do seu chute: "; //"cout <<" Saida de dados pelo terminal
+        cin >> chute; // "cin >>" Entrada de dados pelo terminal
+        cout << "O valor digitado foi " <<chute <<endl;
 
-    if (maior) {
-        cout << "O valor digitado é maior que o número secreto!" <<endl;
-    } else if (menor) {
-        cout << "O valor digitado é menor que o número secreto!" <<endl;
-    } else {
-        cout << "Parabens! Você acertou o número secreto!" <<endl;
+        bool maior = chute > NUMERO_SECRETO;
+        bool menor = chute < NUMERO_SECRETO;
+
+        if (maior) {
+            cout << "O valor digitado é maior que o número secreto!" <<endl;
+        } else if (menor) {
+            cout << "O valor digitado é menor que o número secreto!" <<endl;
+        } else {
+            cout << "Parabens! Você acertou o número secreto!" <<endl;
+            nao_acertou = false;
+        }
     }
+
+    cout << "Número de tentativas " <<tentativas <<endl;
+
+    cout <<"**************************************" << endl;
+    cout <<"*            FIM DE JOGO!            *" << endl;
+    cout <<"**************************************" << endl;
+
 }
