247b243fc23c271af6be08cce917a50d241937c2
rookie-leo <proleonarpa@gmail.com>
2022-09-13 06:48:11 -0300

    Refatorando o código

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -6,6 +6,21 @@ int main() {
     cout <<"* Bem-vindos ao jogo da adivinhação! *" << endl;
     cout <<"**************************************" << endl;
 
-    int numero_secreto = 45;
-    cout <<"O número secreto é " <<numero_secreto<<".Não conte para ningúem!" << endl;
+    const int NUMERO_SECRETO = 45;
+    int chute;
+
+    cout << "Digite o valor do seu chute: "; //"cout <<" Saida de dados pelo terminal
+    cin >> chute; // "cin >>" Entrada de dados pelo terminal
+    cout << "O valor digitado foi " <<chute <<endl;
+
+    bool maior = chute > NUMERO_SECRETO;
+    bool menor = chute < NUMERO_SECRETO;
+
+    if (maior) {
+        cout << "O valor digitado é maior que o número secreto!" <<endl;
+    } else if (menor) {
+        cout << "O valor digitado é menor que o número secreto!" <<endl;
+    } else {
+        cout << "Parabens! Você acertou o número secreto!" <<endl;
+    }
 }
