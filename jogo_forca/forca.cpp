410a7ed9d26dccb14b2622cd8a9c4f83961b6034
rookie-leo <proleonarpa@gmail.com>
2022-09-13 13:40:58 -0300

    Verificando chutes

--- jogo_forca/forca.cpp
+++ jogo_forca/forca.cpp
@@ -2,9 +2,19 @@
 #include <string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute) {
+    for (int i = 0; i < PALAVRA_SECRETA.size(); i++) {
+        if (chute == PALAVRA_SECRETA[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
-    string palavra_secreta = "MELANCIA";
-    cout << palavra_secreta <<endl;
+    cout << PALAVRA_SECRETA <<endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
@@ -16,6 +26,10 @@ int main() {
 
         cin >> chute;
 
-        cout << "Seu chute foi " << chute <<endl;
+        if (letra_existe(chute)) {
+            cout << "Você acertou! Seu chute está na palavra!" <<endl;
+        } else {
+            cout << "Você errou! Seu chute não está na palavra!" <<endl;
+        }
     }
 }
