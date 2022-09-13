a15b460c961ae9fc5450d7579d5b11cb9216a38e
rookie-leo <proleonarpa@gmail.com>
2022-09-13 09:03:18 -0300

    Lendo chutes

--- jogo_forca/forca.cpp
+++ jogo_forca/forca.cpp
@@ -5,4 +5,17 @@ using namespace std;
 int main() {
     string palavra_secreta = "MELANCIA";
     cout << palavra_secreta <<endl;
+
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
+
+    while (nao_acertou && nao_enforcou) {
+        char chute;
+
+        cout << "Chute uma letra ";
+
+        cin >> chute;
+
+        cout << "Seu chute foi " << chute <<endl;
+    }
 }
