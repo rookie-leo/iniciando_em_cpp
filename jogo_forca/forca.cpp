2762c2a2e2da964ff55e36d6708caa52e0f54148
rookie-leo <proleonarpa@gmail.com>
2022-09-14 07:07:55 -0300

    Mostrando chutes errados

--- jogo_forca/forca.cpp
+++ jogo_forca/forca.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
 map<char, bool> chutou;
+vector<char> chutes_errados;
 
 bool letra_existe(char chute) {
     for (char letra : PALAVRA_SECRETA) {
@@ -17,12 +19,22 @@ bool letra_existe(char chute) {
 }
 
 int main() {
+    cout <<"**************************************" << endl;
+    cout <<"*           Jogo da forca!           *" << endl;
+    cout <<"**************************************" << endl;
+
     cout << PALAVRA_SECRETA <<endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while (nao_acertou && nao_enforcou) {
+        cout << "Chutes errados: ";
+        for (char letra : chutes_errados) {
+            cout << letra << " - ";
+        }
+        cout << endl;
+
         for (char letra : PALAVRA_SECRETA) {
             if (chutou[letra]) {
                 cout << letra << " ";
@@ -36,7 +48,6 @@ int main() {
         char chute;
 
         cout << "Chute uma letra ";
-
         cin >> chute;
 
         chutou[chute] = true;
@@ -45,6 +56,8 @@ int main() {
             cout << "Você acertou! Seu chute está na palavra!" <<endl;
         } else {
             cout << "Você errou! Seu chute não está na palavra!" <<endl;
+            chutes_errados.push_back(chute);
         }
+         cout << endl;
     }
 }
